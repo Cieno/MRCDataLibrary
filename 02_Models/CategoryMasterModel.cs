df067dc0c68966337d4582953f4deac374ecbd6d
cieno-naver <cieno@naver.com>
2020-11-11 23:47:03 -0500

    field range -> min/max

--- 02_Models/CategoryMasterModel.cs
+++ 02_Models/CategoryMasterModel.cs
@@ -13,16 +13,24 @@ namespace MRCDataLibrary._02_Models
         //[MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
         //[Range(1, int.MaxValue, ErrorMessage = "You need to select a meal from the list")]
         //[Range(1, 10, ErrorMessage = "You can select up to 10 meals")]
-
+        [Required]
         [DisplayName("Category ID")]
         public string CATEGORY_ID { get; set; }
+
+        [Required]
         [DisplayName("Category Description")]
         public string CATEGORY_DESCR { get; set; }
+
         [DisplayName("Active Y/N")]
         public bool ACTIVE_YN { get; set; }
+
+        [Required]
         [DisplayName("Abbreviation")]
-        [Range(2, 2, ErrorMessage = "The abbreviation is supposed to be 2 upper case characters.")]
+        //[Range(2, 2, ErrorMessage = "The abbreviation is supposed to be 2 upper case characters.")]
+        [MaxLength(2, ErrorMessage = "The abbreviation is supposed to be 2 upper case characters.")]
+        [MinLength(2, ErrorMessage = "The abbreviation is supposed to be 2 upper case characters.")]
         public string ABBR_CD { get; set; }
+
         [DisplayName("Create Date Time")]
         public DateTime CREATE_DT  { get; set; }
     }
